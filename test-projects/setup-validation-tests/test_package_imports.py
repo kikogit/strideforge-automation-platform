bc27b96469c2b6317085055bd1eb0599444ae83d
Kiran Kotian <kotianki@gmail.com>
2026-05-13 09:12:52 -0700

    Add Python package import strategy

---
+++ test-projects/setup-validation-tests/test_package_imports.py
@@ -0,0 +1,23 @@
+
+def test_common_utils_package_import():
+    import common_utils
+
+    assert common_utils is not None
+
+
+def test_ui_core_package_imports():
+    import ui_core
+
+    assert ui_core is not None
+
+
+def test_api_core_package_imports():
+    import api_core
+
+    assert api_core is not None
+
+
+def test_mobile_core_package_imports():
+    import mobile_core
+
+    assert mobile_core is not None
